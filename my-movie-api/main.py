48f555f6a9ffebe3618244be940e523564a2944a
kaezar3600 <cesar.contreras.medina@gmail.com>
2023-03-20 22:26:41 -0500

    validate datatypes

--- my-movie-api/main.py
+++ my-movie-api/main.py
@@ -1,10 +1,33 @@
 
 from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel,Field
+from typing import Optional
 
 app = FastAPI()
 app.title = 'Mi aplicacion con fastAPI'
 app.version = '0.0.1'
+
+class Movie(BaseModel):
+    id: Optional[int] = None
+    title:str = Field( min_length=5, max_length=15)
+    overview:str= Field(min_length=15, max_length=35)
+    year: int = Field( le=2022)
+    ranking:float = Field(le=10)
+    category:str= Field( max_length=33)
+    class Config:
+        schema_extra = {
+            "example":{
+            "id":1,
+            "title":"Mi pelicula",
+            "overview":"Descripcion de la pelicula",
+            "year":2022,
+            "ranking":5.0,
+            "category":"Comedy"
+            }
+        }
+
+
 
 movies = [
     {
@@ -26,10 +49,10 @@ movies = [
     {
       "id":3,
       "title":"Alien",
-      "overview":"Un equipo de militares y un científico parten hacia un planeta desconocido a través de una puerta estelar descubierta en una excavación en Egipto.",
-      "year":2003,
-      "ranking":6.3,
-      "category":"Aventura"
+      "overview":"Una nave carguero recibe una senal de auxilio acudiendo en ayuda sin imaginar que alguien mas abordara ",
+      "year":1977,
+      "ranking":8.3,
+      "category":"Terror"
     },
 ]
 
@@ -56,30 +79,35 @@ def get_movies_by_category(category:str,year:str):
 
 
 @app.post('/movies',tags=['movies'])
-def create_movie(id:int = Body(), title:str=Body(), overview:str=Body(), year:int=Body(), ranking:float=Body(), category:str=Body()):
-    movies.append({
-        "id":id,
-        "title":title,
-        "overview":overview, 
-        "year":year,
-        "ranking":ranking,
-        "category":category
-    })
+def create_movie(movie:Movie):
+    movies.append(movie)
     return movies
 
 @app.put('/movies/{id}', tags=['movies'])
-def update_movie(id:int, title:str=Body(), overview:str=Body(), year:int=Body(), ranking:float=Body(), category:str=Body()):
+def update_movie(id:int, movie:Movie):
     for item in movies:
         if item['id'] == id:
-            item['title'] = title,
-            item['overview'] = overview,
-            item['year'] = year,
-            item['ranking'] = ranking,
-            item['category'] = category,
+            item['title'] = movie.title
+            item['overview'] = movie.overview
+            item['year'] = movie.year
+            item['ranking'] = movie.ranking
+            item['category'] = movie.category
+
+    return movies
+
+@app.delete('/movies/{id}', tags=['movies'])
+def delete_movie(id:int):
+    for movie in movies:
+        if movie["id"] == id:
+            movies.remove(movie)
+
+    return movies
+    
 
 
 
 
+#@app.put('/movies/{id}', tags=['movies'])
 
 
 
